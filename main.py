821ac0a164c4dc0d7a5f9c735e67c5c63cf1e2a6
WhatWR <kul.b@ku.th>
2021-03-28 18:03:16 +0700

    ครับ

--- main.py
+++ main.py
@@ -72,10 +72,10 @@ class PacmanGame(GameApp):
             'A': self.get_pacman_next_direction_function(self.pacman1, DIR_LEFT),
             'S': self.get_pacman_next_direction_function(self.pacman1, DIR_DOWN),
             'D': self.get_pacman_next_direction_function(self.pacman1, DIR_RIGHT),
-            'I': self.get_pacman_next_direction_function(self.pacman1, DIR_UP),
-            'J': self.get_pacman_next_direction_function(self.pacman1, DIR_LEFT),
-            'K': self.get_pacman_next_direction_function(self.pacman1, DIR_DOWN),
-            'L': self.get_pacman_next_direction_function(self.pacman1, DIR_RIGHT),
+            'I': self.get_pacman_next_direction_function(self.pacman2, DIR_UP),
+            'J': self.get_pacman_next_direction_function(self.pacman2, DIR_LEFT),
+            'K': self.get_pacman_next_direction_function(self.pacman2, DIR_DOWN),
+            'L': self.get_pacman_next_direction_function(self.pacman2, DIR_RIGHT),
         }
 
 
